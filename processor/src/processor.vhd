98e99e18c3a563186b74e2213c5a0c3a3db31ec5
Amadeus Chan <greenclouds@foxmail.com>
2017-11-24 09:46:17 +0800

    basic

--- processor/src/processor.vhd
+++ processor/src/processor.vhd
@@ -104,7 +104,105 @@ architecture Behavioral of processor is
 	signal branch_type_tmp, is_branch_tmp, is_jump_tmp: STD_LOGIC;
 	signal branch_relative_reg_data_tmp, branch_target_tmp, jump_target_tmp, instruction_tmp, pc_from_if_tmp: in STD_LOGIC_VECTOR(15 downto 0);
 
+	component register_module is
+	    Port ( instruction_in : in  STD_LOGIC_VECTOR (15 downto 0);
+	           pc_in : in  STD_LOGIC_VECTOR (15 downto 0);
+	           write_back_data_in : in  STD_LOGIC_VECTOR (15 downto 0);
+	           write_back_reg_in : in  STD_LOGIC_VECTOR (3 downto 0);
+	           reg_write_enable : in  STD_LOGIC;
+	           branch_target_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           jump_target_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	    	   is_branch_out, is_jump_out : out  STD_LOGIC;
+	           branch_type_out : out  STD_LOGIC;
+	           branch_relative_reg_data_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           is_hazard_1 : in  STD_LOGIC;
+	           is_hazard_2 : in  STD_LOGIC;
+	           forward_data_1 : in  STD_LOGIC_VECTOR (15 downto 0);
+	           forward_data_2 : in  STD_LOGIC_VECTOR (15 downto 0);
+	           pc_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           wb_src_out : out  STD_LOGIC_VECTOR (2 downto 0);
+	           mem_data_from_reg_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           wb_data_from_reg_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           immediate_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           write_back_reg_out : out  STD_LOGIC_VECTOR (3 downto 0);
+	           reg_write_enable_out : out  STD_LOGIC;
+	           op_code_out : out  STD_LOGIC_VECTOR (3 downto 0);
+	           operand1_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           operand2_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           cin_out : out  STD_LOGIC;
+	           mem_enable_out : out  STD_LOGIC;
+	           mem_read_out : out  STD_LOGIC;
+	           mem_write_out : out  STD_LOGIC;
+		   clk, rst: in STD_LOGIC);
+	end component;
+
+	signal write_back_data_to_wb_tmp: STD_LOGIC_VECTOR(15 downto 0);
+	signal write_back_reg_to_wb_tmp: STD_LOGIC_VECTOR(3 downto 0);
+	signal write_back_enable_to_wb_tmp: STD_LOGIC;
+	signal branch_target_from_id_tmp, jump_target_from_id_tmp, branch_relative_reg_data_from_id_tmp: STD_LOGIC_VECTOR(15 downto 0);
+	signal is_branch_from_id_tmp, is_jump_from_id_tmp, branch_type_from_id_tmp: STD_LOGIC;
+	signal is_hazard_1_tmp, is_hazard_2_tmp: STD_LOGIC;
+	signal forward_data_1_tmp, forward_data_2_tmp: STD_LOGIC_VECTOR(15 downto 0);
+	signal pc_from_id_tmp, mem_data_from_reg_from_id_tmp, wb_data_from_reg_from_id_tmp, immediate_from_id_tmp, operand1_from_id_tmp, operand2_from_id_tmp: STD_LOGIC_VECTOR(15 downto 0);
+	signal wb_src_from_id_tmp: STD_LOGIC_VECTOR(2 downto 0);
+	signal write_back_reg_from_id_tmp, op_code_from_id_tmp: STD_LOGIC_VECTOR(3 downto 0);
+	signal reg_wrrite_enable_from_id_tmp, cin_from_id_tmp, mem_enable_from_id_tmp, mem_read_from_id_tmp, mem_write_from_id_tmp: STD_LOGIC;
+
+	component IF_ID_regs is
+	    Port ( bubble : in  STD_LOGIC;
+	           stall : in  STD_LOGIC;
+	           instruction_in : in  STD_LOGIC_VECTOR (15 downto 0);
+	           pc_in : in  STD_LOGIC_VECTOR (15 downto 0);
+	           instruction_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           pc_out : out  STD_LOGIC_VECTOR (15 downto 0);
+	           clk : in  STD_LOGIC;
+	           rst : in  STD_LOGIC);
+	end component;
+
+	signal bubble_to_
+	
 begin
+
+	IF_ID_regs_imp: IF_ID_regs 
+	port map(
+	bubble => 
+	);
+
+
+	register_module_imp: register_module 
+	port map(
+		instruction_in => instruction_tmp,
+		pc_in => pc_from_if_tmp,
+		write_back_data_in => write_back_data_to_wb_tmp,
+		write_back_reg_in => write_back_reg_to_wb_tmp,
+		reg_write_enable => write_back_enable_to_wb_tmp,
+		branch_target_out => branch_target_from_id_tmp,
+		jump_target_out => jump_target_from_id_tmp,
+		is_branch_out => is_branch_from_id_tmp,
+		is_jump_out => is_jump_from_id_tmp,
+		branch_type_out => branch_type_from_id_tmp,
+		branch_relative_reg_data_out => branch_relative_reg_data_from_id_tmp,
+		is_hazard_1 => is_hazard_1_tmp,
+		is_hazard_2 => is_hazard_2_tmp,
+		forward_data_1 => forward_data_1_tmp,
+		forward_data_2 => forward_data_2_tmp,
+		pc_out => pc_from_id_tmp,
+		wb_src_out => wb_src_from_id_tmp,
+		mem_data_from_reg_out => mem_data_from_reg_from_id_tmp,
+		wb_data_from_reg_out => wb_data_from_reg_from_id_tmp,
+		immediate_out => immediate_from_id_tmp,
+		write_back_reg_out => write_back_reg_from_id_tmp,
+		reg_write_enable_out => reg_wrrite_enable_from_id_tmp,
+		op_code_out => op_code_from_id_tmp,
+		operand1_out => operand1_from_id_tmp,
+		operand2_out => operand2_from_id_tmp,
+		cin_out => cin_from_id_tmp,
+		mem_enable_out => mem_enable_from_id_tmp,
+		mem_read_out => mem_read_from_id_tmp,
+		mem_write_out => mem_write_from_id_tmp,
+		clk => clk,
+		rst => rst
+	);
 
 	instruction_fetch_module_imp: instruction_fetch_module 
 	port map(
@@ -125,7 +223,7 @@ begin
 		jump_target_in => jump_target_tmp,
 		instruction_out => instruction_tmp,
 		pc_out => pc_from_if_tmp
-		);
+	);
 	
 end Behavioral;
 
