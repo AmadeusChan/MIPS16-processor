54c2365de5cbe20819a76d78164d4e4753b1ab20
高信龙一 <gxly15@mails.tsinghua.edu.cn>
2017-12-07 02:48:42 +0800

    Update 35M

--- processor/src/UAL_hazard_detector.vhd
+++ processor/src/UAL_hazard_detector.vhd
@@ -49,33 +49,33 @@ end UAL_hazard_detector;
 
 architecture Behavioral of UAL_hazard_detector is
 
-	type state_type is (detecting, detected);
-	signal state: state_type := detecting;
+--	type state_type is (detecting, detected);
+--	signal state: state_type := detecting;
 
 	signal is_ual_hazard_1_tmp, is_ual_hazard_2_tmp: STD_LOGIC;
 
 begin
 
-	process (clk, rst)
-	begin
-		if rst = '0' then
-			state <= detecting;
-			--is_ual_hazard_1 <= '0';
-			--is_ual_hazard_2 <= '0';
-		elsif clk'event and clk = '1' then 
-			if state = detecting then
-				if is_ual_hazard_1_tmp = '1' or is_ual_hazard_2_tmp = '1' then
-					state <= detected;
-					--is_ual_hazard_1 <= is_ual_hazard_1_tmp;
-					--is_ual_hazard_2 <= is_ual_hazard_2_tmp;
-				end if;
-			else 
-				state <= detecting;
-				--is_ual_hazard_1 <= '0';
-				--is_ual_hazard_2 <= '0';
-			end if;
-		end if;
-	end process;
+--	process (clk, rst)
+--	begin
+--		if rst = '0' then
+--			state <= detecting;
+--			--is_ual_hazard_1 <= '0';
+--			--is_ual_hazard_2 <= '0';
+--		elsif clk'event and clk = '1' then 
+--			if state = detecting then
+--				if is_ual_hazard_1_tmp = '1' or is_ual_hazard_2_tmp = '1' then
+--					state <= detected;
+--					--is_ual_hazard_1 <= is_ual_hazard_1_tmp;
+--					--is_ual_hazard_2 <= is_ual_hazard_2_tmp;
+--				end if;
+--			else 
+--				state <= detecting;
+--				--is_ual_hazard_1 <= '0';
+--				--is_ual_hazard_2 <= '0';
+--			end if;
+--		end if;
+--	end process;
 --
 --	process (is_ual_hazard_1_tmp, state)
 --	begin 
@@ -97,32 +97,33 @@ begin
 --		end case;
 --	end process;
 
-	process (is_ual_hazard_1_tmp, is_ual_hazard_2_tmp)
-	begin
-		bubble <= is_ual_hazard_1_tmp or is_ual_hazard_2_tmp;
-		stall <= is_ual_hazard_1_tmp or is_ual_hazard_2_tmp;
-	end process;
 
-	process (read_reg1, read_reg2, write_back_reg, reg_write_enable, mem_read, state)
-	begin 
-		--if state = detecting then 
-			if mem_read = enable and reg_write_enable = enable then
-					if read_reg1 = write_back_reg then
-							is_ual_hazard_1_tmp <= '1';
-							is_ual_hazard_2_tmp <= '0';
-						elsif read_reg2 = write_back_reg then
-								is_ual_hazard_1_tmp <= '0';
-								is_ual_hazard_2_tmp <= '1';
-							else 
-								is_ual_hazard_1_tmp <= '0';
-								is_ual_hazard_2_tmp <= '0';
-							end if;
-				else 
-					is_ual_hazard_1_tmp <= '0';
-					is_ual_hazard_2_tmp <= '0';
-				end if;
-		--end if;
-	end process;
+	bubble <= is_ual_hazard_1_tmp or is_ual_hazard_2_tmp;
+	stall <= is_ual_hazard_1_tmp or is_ual_hazard_2_tmp;
+	
+	is_ual_hazard_1_tmp <= '1' when mem_read = enable and reg_write_enable = enable and read_reg1 = write_back_reg else '0';
+	is_ual_hazard_2_tmp <= '1' when mem_read = enable and reg_write_enable = enable and read_reg2 = write_back_reg else '0';
+
+--	process (read_reg1, read_reg2, write_back_reg, reg_write_enable, mem_read, state)
+--	begin 
+--		--if state = detecting then 
+--			if mem_read = enable and reg_write_enable = enable then
+--					if read_reg1 = write_back_reg then
+--							is_ual_hazard_1_tmp <= '1';
+--							is_ual_hazard_2_tmp <= '0';
+--						elsif read_reg2 = write_back_reg then
+--								is_ual_hazard_1_tmp <= '0';
+--								is_ual_hazard_2_tmp <= '1';
+--							else 
+--								is_ual_hazard_1_tmp <= '0';
+--								is_ual_hazard_2_tmp <= '0';
+--							end if;
+--				else 
+--					is_ual_hazard_1_tmp <= '0';
+--					is_ual_hazard_2_tmp <= '0';
+--				end if;
+--		--end if;
+--	end process;
 
 
 end Behavioral;
